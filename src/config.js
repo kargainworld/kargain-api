9077b06e938e98c251b9cc67dd5b01cc2a4ed5dc
rayConnection <georgy@rayconnection.co.uk>
2020-11-07 23:49:24 +0100

    "config path"

--- src/config.js
+++ src/config.js
@@ -20,7 +20,7 @@ module.exports = {
 	'http://localhost:3000',
 	'http://localhost:5000',
 	'https://kargain.com',
-	'https://api.kargain.com/v1',
+	'https://api.kargain.com',
 	'https://kargain-api-nine.vercel.app',
 	'https://kargain-api.kargainworld.vercel.app/',
 	'https://kargain-app.kargainworld.vercel.app'
