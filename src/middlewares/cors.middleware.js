8bfdae13608a381552e4b8bdd2f53cbc240d25fe
rayConnection <georgy@rayconnection.co.uk>
2020-11-07 23:58:23 +0100

    "cors"

--- src/middlewares/cors.middleware.js
+++ src/middlewares/cors.middleware.js
@@ -1,21 +1,33 @@
 const config = require('../config')
 const Errors = require('../utils/errors')
+const logger = require('../services/logger')
 
 function corsOptions (allowCredentials = false) {
-    return (req, res, next) => {
-        const origin = req.headers.origin
-        if (config.whileListDomains.indexOf(origin) !== -1) {
-            res.header('Access-Control-Allow-Origin', origin)
-        } else {
-            res.header('Access-Control-Allow-Origin', true)
-        }
-        res.header('Access-Control-Allow-Credentials', allowCredentials)
-        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
-        next()
+    return {
+	allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization'],
+	preflightContinue: true,
+	credentials: true,
+	 origin: function (origin, callback) {
+	     if (config.whileListDomains.indexOf(origin) !== -1) {
+		 callback(null, allowCredentials ? origin : true)
+	     } else {
+		 callback(Errors.UnAuthorizedError('Not allowed by CORS'))
+	     }
+	 }
     }
 }
 
-const manualCors = corsOptions(true)
+const manualCors = (req, res, next) => {
+    const origin = req.headers.origin
+    if (config.whileListDomains.indexOf(origin) !== -1){
+	res.header('Access-Control-Allow-Origin', origin)
+    } else {
+	res.header('Access-Control-Allow-Origin', true)
+    }
+    res.header('Access-Control-Allow-Credentials', true)
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+    next()
+}
 
 const clientCors = corsOptions(false)
 const authedCors = corsOptions(true)
