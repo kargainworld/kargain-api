8dddb92a73e71e40870e1f1f4d7bd0f8dbb5d481
• raYCønnectiøn <67691938+rayconnection@users.noreply.github.com>
2021-05-28 17:48:44 +0200

    Delete dist directory

--- dist/components/filters/announcesSorterMapper.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-module.exports = {
-  MAKE: 'manufacturer.make.value',
-  PRICE: 'price',
-  MILEAGE: 'mileage',
-  DATE: 'createdAt',
-  POSITION: 'distanceRadius'
-};
--- dist/models/tag.model.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var mongoose = require('mongoose');
-
-var TagSchema = new mongoose.Schema({
-  name: String
-}); // Export mongoose model
-
-module.exports = mongoose.model('Tag', TagSchema);
