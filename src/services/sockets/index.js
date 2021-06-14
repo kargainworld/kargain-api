1430d477d26e52e228a75900a9239b3275e6ab1e
bigluckypal010203 <bigluckypal010203@gmail.com>
2021-06-14 21:27:08 +0900

    add private chat and online status

--- src/services/sockets/index.js
+++ src/services/sockets/index.js
@@ -1,5 +1,7 @@
 const Server = require('socket.io')
 const notificationsController = require('../../controllers/notifications.controller')
+const conversationsController = require('../../controllers/conversations.controller')
+const notifier = require('../../components/notifications/notifier')
 
 Socket = {}
 Socket.ids = new Array()
@@ -19,12 +21,14 @@ Socket.init = (httpServer) => {
         }
 
         socket.userId = userId
+        socket.join(socket.userId)
         next()
     })
 
     Socket.io.on('connection', (socket) => {
         console.log('Connected', socket.handshake.auth)
-
+        // Socket.io.broadcast.emit('SET_ONLINE_STATUS',)
+        Socket.sendBroadCast('SET_ONLINE_STATUS', socket)
         socket.emit('PING', 'FROM SERVER')
 
         socket.on('connect_error', (err) => {
@@ -34,7 +38,32 @@ Socket.init = (httpServer) => {
         socket.on('OPENED_NOTIFICATION', () => {
             notificationsController.setOpenNotification({ id: socket.userId })
         })
+
+        socket.on('disconnect', () => {
+            Socket.sendBroadCast('SET_OFFLINE_STATUS', socket)
+        })
+
+        socket.on('PRIVATE_MESSAGE', async ({ message, to }) => {
+            try {
+                const result = await conversationsController.postConversationMessageFromSocket(socket.userId, to, message)
+                if (result) {
+                    socket.to(to).emit('PRIVATE_MESSAGE', {
+                        content: message,
+                        from: socket.userId
+                    })
+                }
+                // console.log(this)
+                notifier.postNotification({ uid: to, message, action: '', socket })
+            } catch (error) {
+                console.trace(error)
+            }
+
+        })
     })
+}
+
+Socket.sendBroadCast = (type, target) => {
+    target.broadcast.emit(type, target.userId)
 }
 
 Socket.sendMessage = (msgType, msg, userId) => {
